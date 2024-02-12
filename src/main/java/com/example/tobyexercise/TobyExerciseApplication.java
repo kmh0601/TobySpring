ef958536e81d3b47af2044c27c7282728136197f
kmh0601 <pongkmh@naver.com>
2024-02-12 23:32:46 +0900

    feat: chapter1 spring application

---
+++ src/main/java/com/example/tobyexercise/TobyExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobyexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobyExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tobyexercise/TobyExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobyexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
